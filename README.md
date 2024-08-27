575a63356d2924310e573c273e88ef5062d1c3ba
Luis Corredor <lcorredor2@udi.edu.co>
2024-08-27 08:51:47 -0500

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyecto_grado_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/proyecto_grado_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto_grado_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
